1a77345b256a7621aaefa51c0f7f55c53904a1bb
Vestamitts <vestamitts@cock.li>
2022-11-28 01:47:06 -0800

    Initial commit

---
+++ ImprovedFilter/ImprovedFilterPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ImprovedFilter
+{
+	[HarmonyPatch(typeof(ItemInstance))]
+	public class ImprovedFilterPatch
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch("DropItem")]
+		public static void DropItemPostfix(Vector2 position, Vector2 floorPosition, Vector2 force, ItemInstance __instance)
+		{
+			bool inInventory = __instance.State.HasFlag(ItemInstanceState.InInventory);
+			if (inInventory) return;
+
+			bool passedFilter = Singleton<ItemFilterManager>.instance.PassedFilter(__instance);
+			if (!passedFilter) __instance.DestroyItem();
+		}
+	}
+}
---
+++ ImprovedFilter/ModLoader.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+
+namespace ImprovedFilter
+{
+	public class ModLoader : IModLoader
+	{
+		public void OnCreated()
+		{
+			Harmony harmony = new Harmony("com.vestamitts.improved-filter");
+			harmony.PatchAll();
+		}
+
+		public void OnGameLoaded(LoadMode mode)
+		{
+			
+		}
+
+		public void OnGameUnloaded()
+		{
+			
+		}
+
+		public void OnReleased()
+		{
+			
+		}
+	}
+}
