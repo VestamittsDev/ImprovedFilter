dc7c0bb35b1ee96e692d42552de5b8df5c7edc67
Vestamitts <vestamitts@cock.li>
2022-11-30 17:38:00 -0800

    Only apply to equips and glyphs

--- ImprovedFilter/ImprovedFilterPatch.cs
+++ ImprovedFilter/ImprovedFilterPatch.cs
@@ -8,13 +8,44 @@ namespace ImprovedFilter
 	{
 		[HarmonyPrefix]
 		[HarmonyPatch("DropItem")]
-		public static void DropItemPostfix(Vector2 position, Vector2 floorPosition, Vector2 force, ItemInstance __instance)
+		public static bool DropItemPrefix(Vector2 position, Vector2 floorPosition, Vector2 force, ItemInstance __instance)
 		{
+			// Do not apply to items dropped from inventory
 			bool inInventory = __instance.State.HasFlag(ItemInstanceState.InInventory);
-			if (inInventory) return;
+			if (inInventory)
+			{
+				return true;
+			}
 
+			// Only apply to equips and glyphs
+			if (!IsEquipment(__instance))
+			{
+				return true;
+			}
+
+			// Check against current item filter, if it does not pass destroy and do not execute DropItem
 			bool passedFilter = Singleton<ItemFilterManager>.instance.PassedFilter(__instance);
-			if (!passedFilter) __instance.DestroyItem();
+			if (!passedFilter)
+			{
+				__instance.DestroyItem();
+			}
+
+			return false;
 		}
+
+		#region Helpers
+		private static bool IsEquipment(ItemInstance item)
+		{
+			bool isMainHand = item.Attributes.HasFlag(ItemAttributes.MainHand);
+			bool isOffHand = item.Attributes.HasFlag(ItemAttributes.OffHand);
+			bool isHead = item.Attributes.HasFlag(ItemAttributes.Head);
+			bool isChest = item.Attributes.HasFlag(ItemAttributes.Chest);
+			bool isAmulet = item.Attributes.HasFlag(ItemAttributes.Amulets);
+			bool isGlyph = item.Attributes.HasFlag(ItemAttributes.SimpleGlyph);
+			bool isCompoundGlyph = item.Attributes.HasFlag(ItemAttributes.CompoundGlyph);
+
+			return isMainHand || isOffHand || isHead || isChest || isAmulet || isGlyph || isCompoundGlyph;
+		}
+		#endregion
 	}
 }
